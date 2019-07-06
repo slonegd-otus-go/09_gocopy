616c60c4618f70bcca1bb3655aff1ca715d30f6d
slonegd <slonkdv@mail.ru>
2019-07-06 12:39:49 +0300

    copy file with offset and limit

--- internal/process.go
+++ internal/process.go
@@ -1,7 +1,21 @@
 package internal
 
-import "io"
+import (
+	"errors"
+	"io"
+)
 
-func Process(reader io.Reader, writer io.Writer, offset, limit int) {
-
+func Process(reader io.Reader, writer io.Writer, offset, limit int) error {
+	if offset != 0 {
+		seeker, ok := reader.(io.Seeker)
+		if !ok {
+			return errors.New("не могу выполнить смещение")
+		}
+		err := seeker.Seek(int64(offset), io.SeekStart)
+		if err != nil {
+			return err
+		}
+	}
+	io.CopyN(writer, reader, int64(limit))
+	return nil
 }
