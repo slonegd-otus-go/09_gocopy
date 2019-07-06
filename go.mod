3209759d02b2fb0c3ddcd831db92b94da00f4e3f
slonegd <slonkdv@mail.ru>
2019-07-06 13:32:21 +0300

    happy path test

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module github.com/slonegd-otus-go/09_gocopy
+
+require github.com/stretchr/testify v1.3.0
---
+++ internal/process_test.go
@@ -0,0 +1,45 @@
+package internal
+
+import (
+	"bytes"
+	"io"
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestProcess(t *testing.T) {
+	var gotProgress string
+	tests := []struct {
+		name         string
+		reader       io.Reader
+		offset       int
+		limit        int
+		callback     func(progress int)
+		wantWriter   string
+		wantProgress string
+		wantErr      bool
+	}{
+		{
+			name:   "happy path",
+			reader: strings.NewReader("test"),
+			limit:  4,
+			callback: func(progress int) {
+				gotProgress += strconv.Itoa(progress) + "%"
+			},
+			wantWriter:   "test",
+			wantProgress: "0%25%50%75%100",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotProgress = ""
+			writer := &bytes.Buffer{}
+			Process(tt.reader, writer, tt.offset, tt.limit, tt.callback)
+			assert.Equal(t, tt.wantWriter, writer.String())
+			assert.Equal(t, tt.wantWriter, writer.String())
+		})
+	}
+}
