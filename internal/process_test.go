4f6ceb10c994883efafc7b5d057d00ccbc2ccc31
slonegd <slonkdv@mail.ru>
2019-07-07 15:23:59 +0300

    тесты на обработку ошибок

--- internal/process_test.go
+++ internal/process_test.go
@@ -2,6 +2,8 @@ package internal
 
 import (
 	"bytes"
+	"errors"
+	"fmt"
 	"io"
 	"strconv"
 	"strings"
@@ -9,6 +11,30 @@ import (
 
 	"github.com/stretchr/testify/assert"
 )
+
+type notSeeker struct{}
+
+func (notSeeker) Read([]byte) (int, error) {
+	return 0, nil
+}
+
+type errorSeeker struct{}
+
+func (errorSeeker) Read([]byte) (int, error) {
+	return 0, nil
+}
+func (errorSeeker) Seek(int64, int) (int64, error) {
+	return 0, errors.New("seek error")
+}
+
+type errorRead struct{}
+
+func (errorRead) Read([]byte) (int, error) {
+	return 0, errors.New("read error")
+}
+func (errorRead) Seek(int64, int) (int64, error) {
+	return 0, nil
+}
 
 func TestProcess(t *testing.T) {
 	var gotProgress string
@@ -18,64 +44,106 @@ func TestProcess(t *testing.T) {
 	tests := []struct {
 		name         string
 		reader       io.Reader
+		writer       io.Writer
 		offset       int
 		limit        int
 		callback     func(progress int)
 		wantWriter   string
 		wantProgress string
-		// wantErr      bool
+		wantErr      string
 	}{
 		{
-			name:   "happy path",
-			reader: strings.NewReader("test"),
-			limit:  4,
-			callback: callback,
+			name:         "happy path",
+			reader:       strings.NewReader("test"),
+			writer:       &bytes.Buffer{},
+			limit:        4,
+			callback:     callback,
 			wantWriter:   "test",
 			wantProgress: "0% 25% 50% 75% 100% ",
 		},
 		{
-			name:   "happy path with offset",
-			reader: strings.NewReader("test"),
-			offset: 2,
-			limit:  2,
-			callback: callback,
+			name:         "happy path with offset",
+			reader:       strings.NewReader("test"),
+			writer:       &bytes.Buffer{},
+			offset:       2,
+			limit:        2,
+			callback:     callback,
 			wantWriter:   "st",
 			wantProgress: "0% 50% 100% ",
 		},
 		{
-			name:   "limit < lehgth",
-			reader: strings.NewReader("test"),
-			limit:  3,
-			callback: callback,
+			name:         "limit < lehgth",
+			reader:       strings.NewReader("test"),
+			writer:       &bytes.Buffer{},
+			limit:        3,
+			callback:     callback,
 			wantWriter:   "tes",
 			wantProgress: "0% 33% 66% 100% ",
 		},
 		{
-			name:   "limit > lehgth",
-			reader: strings.NewReader("test"),
-			limit:  5,
-			callback: callback,
+			name:         "limit > lehgth",
+			reader:       strings.NewReader("test"),
+			writer:       &bytes.Buffer{},
+			limit:        5,
+			callback:     callback,
 			wantWriter:   "test",
 			wantProgress: "0% 20% 40% 60% 80% 100% ",
 		},
 		{
-			name:   "offset > lehgth",
-			reader: strings.NewReader("test"),
-			limit:  5,
-			offset: 5,
-			callback: callback,
+			name:         "offset > lehgth",
+			reader:       strings.NewReader("test"),
+			writer:       &bytes.Buffer{},
+			limit:        5,
+			offset:       5,
+			callback:     callback,
 			wantWriter:   "",
 			wantProgress: "0% 100% ",
+		},
+		{
+			name:         "no Seeker error",
+			reader:       notSeeker{},
+			writer:       &bytes.Buffer{},
+			limit:        5,
+			offset:       5,
+			callback:     callback,
+			wantWriter:   "",
+			wantProgress: "",
+			wantErr:      "не могу выполнить смещение",
+		},
+		{
+			name:         "seeker error",
+			reader:       errorSeeker{},
+			writer:       &bytes.Buffer{},
+			limit:        5,
+			offset:       5,
+			callback:     callback,
+			wantWriter:   "",
+			wantProgress: "",
+			wantErr:      "seek error",
+		},
+		{
+			name:         "read error",
+			reader:       errorRead{},
+			writer:       &bytes.Buffer{},
+			limit:        5,
+			offset:       5,
+			callback:     callback,
+			wantWriter:   "",
+			wantProgress: "0% ",
+			wantErr:      "ошибка копирования: read error",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			gotProgress = ""
-			writer := &bytes.Buffer{}
 			//nolint
-			Process(tt.reader, writer, tt.offset, tt.limit, tt.callback)
+			err := Process(tt.reader, tt.writer, tt.offset, tt.limit, tt.callback)
+			if len(tt.wantErr) != 0 {
+				assert.Error(t, err)
+				assert.Equal(t, tt.wantErr, err.Error())
+			}
 			//nolint
-			assert.Equal(t, tt.wantWriter, writer.String())
+			assert.Equal(t, tt.wantWriter, tt.writer.(fmt.Stringer).String())
 			//nolint
 			assert.Equal(t, tt.wantProgress, gotProgress)
 		})
